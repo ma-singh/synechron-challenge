15a1055c23b7be9e8b250c00befa9d997d7ff360
ma-singh <masingh@fullsail.edu>
2018-10-15 16:06:07 -0400

    attempting to properly implement the addBook function

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { Router } from '@angular/router';
 
@@ -21,6 +22,7 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
+    FormsModule,
     UiModule
   ],
   providers: [],
